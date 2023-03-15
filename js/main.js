2d3736a124a81196e102aed5bb3c7236be7ec3c8
eugeniarossi <eugenia.rossi@outlook.com>
2023-03-15 16:32:26 +0100

    milestone 1

--- js/main.js
+++ js/main.js
@@ -4,7 +4,24 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        
+        return {
+            title: 'Todo List',
+            todoList: [
+                {
+                    text: "Completare l'esercizio",
+                    done: false
+                }, {
+                    text: 'Fare la spesa',
+                    done: true
+                }, {
+                    text: 'Annaffiare le piante',
+                    done: true
+                }, {
+                    text: "Pulire l'acquario",
+                    done: false
+                }
+            ]
+        }
     },
     methods: {
 
